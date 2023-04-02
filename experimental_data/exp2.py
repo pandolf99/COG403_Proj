2b38e1660a622034297e83b2f371d209ea4e96f9
Pandolf <pandolf99@gmail.com>
2023-04-02 11:11:08 -0400

    plot experimental data for exp2

--- experimental_data/exp2.py
+++ experimental_data/exp2.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
-
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+from matplotlib.offsetbox import AnchoredText
 
 alphas = {
         "c1": 0.01,
@@ -9,6 +11,27 @@ alphas = {
         "c4": 0.75,
         "c5": 0.99
         }
+
+
+def plot_exp2(x, y):
+    """Plot bar graphs for experiment 2
+    """
+    #TODO add error bars
+    text1 = r'Condition1: $\kappa = 10, \theta = 100 $' + "\n"
+    text2 = r'Condition2: $\kappa = 17.77, \theta = 100$'+ "\n" 
+    text3 = r'Condition3: $\kappa = 20, \theta = 100 $'+ "\n"
+    text4 = r'Condition4: $\kappa = 22, \theta = 100 $'+ "\n"
+    text5 = r'Condition5: $\kappa = 30, \theta = 100 $'
+    anchor = AnchoredText(text1+text2+text3+text4+text5, loc="upper right")
+    title = "Model predictions with alpha based on gamma distributions" 
+    fig, ax = plt.subplots()
+    ax.bar(x, y)
+    ax.yaxis.set_major_locator(ticker.MultipleLocator(0.1))
+    ax.set_ylabel('P(c->e|e,c,a)')
+    ax.set_ylim(0, 1)
+    ax.set_title(title)
+    ax.add_artist(anchor)
+    plt.show()
 
 #Function to pass to apply
 #and then update rating
@@ -39,7 +62,11 @@ def cleanData():
         "c4":df_c4["Rating"],
         "c5":df_c5["Rating"],
          })
-    print(df_ratings.apply(np.mean) / 10)
+    return df_ratings
 
 if __name__ == "__main__":
+    labels = ["Condition1", "Condition2", "Condition3", "Condition4", "Condition5"]
+    df_ratings = cleanData()
+    means = np.asarray(df_ratings.apply(np.mean)) / 10
+    plot_exp2(labels, means)
     cleanData()
